c313a50abef2d3d914c2d03aa41ff02f28d2124d
Arkady Yuzhakov <ark.yuzhakov@gmail.com>
2022-09-06 15:50:35 +0500

    Edit operate function. Result now rounded to 2 decimal places

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
-let displayValue;
+let displayValue; 
 
 let calcDisplay = document.querySelector('.display');
 
-let buttons = document.querySelectorAll('.digit');
+let buttons = document.querySelectorAll('.digit'); // digits and ',' buttons
 
+//** Add event listeners for digits and ',' buttons. Display according value on calculator display */
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         displayValue = calcDisplay.textContent;
-        if (displayValue == 0) {
-            calcDisplay.textContent = '';
-        }
+        if (displayValue == 0 && button.textContent !== ',') calcDisplay.textContent = '';
+        if (displayValue.includes(',') && button.textContent == ',') return;
         calcDisplay.textContent += button.textContent;
     });
 });
 
+//** Function for basic operations */
 function operate(num1, num2, operator) {
     switch (operator) {
         case '+':
-            return num1 + num2;
+            return 0 + (num1 + num2).toFixed(2);
         case '-':
-            return num1 - num2;
+            return 0 + (num1 - num2).toFixed(2);
         case '*':
-            return num1 * num2;
+            return 0 + (num1 * num2).toFixed(2);
         case '/':
             if (num2 === 0) {
                 return 'undefined/infinity';
-            } else return num1 / num2;
+            } else return 0 + (num1 / num2).toFixed(2);
     }
 };
