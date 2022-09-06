91dcd2ce2eaeba732aaea05f9fbb14e273485fac
Arkady Yuzhakov <ark.yuzhakov@gmail.com>
2022-09-06 16:29:38 +0500

    Add object with control functions, event listeners for control buttons

--- js/script.js
+++ js/script.js
@@ -3,14 +3,45 @@ let displayValue;
 let calcDisplay = document.querySelector('.display');
 
 let buttons = document.querySelectorAll('.digit'); // digits and ',' buttons
+let controls = document.querySelectorAll('.control'); // AC, +/-, % and 'del' buttons
 
-//** Add event listeners for digits and ',' buttons. Display according value on calculator display */
+//** Event listeners for digits and ',' buttons. Display according value on calculator display */
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         displayValue = calcDisplay.textContent;
         if (displayValue == 0 && button.textContent !== ',') calcDisplay.textContent = '';
         if (displayValue.includes(',') && button.textContent == ',') return;
         calcDisplay.textContent += button.textContent;
+    });
+});
+
+//** Object with control buttons */
+let controlButtons = {
+    'AC': function () {
+        calcDisplay.textContent = 0;
+    },
+    '+/-': function () {
+        displayValue = calcDisplay.textContent;
+        calcDisplay.textContent = Number(displayValue) * (-1);
+    },
+    '%': function () {
+        displayValue = calcDisplay.textContent;
+        calcDisplay.textContent = Number(displayValue)/100;
+    },
+    'del': function () {
+        displayValue = calcDisplay.textContent;
+        if (displayValue.length > 1) {
+            calcDisplay.textContent = displayValue.substring(0, displayValue.length - 1);
+        } else controlButtons.AC();
+    },
+}
+
+//** Event listeners for control buttons  */
+controls.forEach((control) => {
+    control.addEventListener('click', () => {
+        let controlAction = control.textContent;
+        controlButtons[controlAction]();
+        console.log(controlButtons[controlAction]);
     });
 });
 
