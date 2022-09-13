f629d0593beeee088e750551fe5f3956b89faeeb
Arkady Yuzhakov <ark.yuzhakov@gmail.com>
2022-09-13 11:02:53 +0500

    Rewrite all with simple functions. It works somehow now =)

--- js/script.js
+++ js/script.js
@@ -1,49 +1,113 @@
-let counter = 0;
-let result;
-let num1 = undefined;
-let num2 = undefined;
+let result = undefined;
+let operatorValue = undefined;
+let displayValue = document.querySelector('.display');
+let newInputState = 0;
 
-let calcDisplay = document.querySelector('.display');
+function add(num1, num2) {
+    return Number(Number(num1) + Number(num2));
+}
+
+function subtract(num1, num2) {
+    return Number(num1 - num2);
+}
+
+function multiply(num1, num2) {
+    return Number(num1 * num2);
+}
+
+function divide(num1, num2) {
+    return Number(num1 / num2);
+}
+
+function operate(num1, num2, operator) {
+    return Number(operator(num1, num2));
+}
+
+const digits = document.querySelectorAll('.digit');
+
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        if (displayValue.textContent.includes('.') && digit.textContent.includes('.')) return;
+        if (displayValue.textContent === '0' && digit.textContent !== '.'
+            || newInputState == 0 && digit.textContent !== '.') {
+            displayValue.textContent = digit.textContent;
+        } else {
+            displayValue.textContent += digit.textContent;
+        }
+        newInputState = 1;
+    })
+})
 
 const controls = document.querySelectorAll('.control');
-const inputSymbols = document.querySelectorAll('.digit');
-
-function checkDisplay() {
-    return calcDisplay.textContent;
-};
-
-const calcFunctions = {
-    'AC': function () {
-        calcDisplay.textContent = 0;
-    },
-    '+/-': function () {
-        calcDisplay.textContent = Number(checkDisplay()) * (-1);
-    },
-    '%': function () {
-        calcDisplay.textContent = Number(checkDisplay()) / 100;
-    },
-    'del': function () {
-        if (checkDisplay().length > 1 && Number(checkDisplay().substring(0, checkDisplay().length - 1)) !== 0) {
-            calcDisplay.textContent = checkDisplay().substring(0, checkDisplay().length - 1);
-        } else calcFunctions.AC();
-    },
-};
 
 controls.forEach((control) => {
     control.addEventListener('click', () => {
-        let controlAction = control.textContent;
-        calcFunctions[controlAction]();
-    });
-});
+        switch (control.textContent) {
+            case 'AC':
+                result = undefined;
+                operatorValue = undefined;
+                newInputState = 0;
+                displayValue.textContent = 0;
+                break;
+            case '+/-':
+                displayValue.textContent = Number(displayValue.textContent) * (-1);
+                break;
+            case '%':
+                displayValue.textContent = Number(displayValue.textContent) / 100;
+                break;
+            case 'del':
+                if (displayValue.textContent.length > 1) {
+                    displayValue.textContent = displayValue.textContent.substring(0, displayValue.textContent.length - 1);
+                } else {
+                    displayValue.textContent = 0;
+                    result = undefined;
+                    operatorValue = undefined;
+                }
+                break;
+        }
+    })
+})
 
-inputSymbols.forEach((symbol) => {
-    symbol.addEventListener('click', () => {
-        let buttonValue = symbol.textContent;
-        if (checkDisplay().includes('.') && buttonValue == '.') return;
-        if (Number(checkDisplay()) === 0 && buttonValue !== '.' && !checkDisplay().includes('.')) {
-            calcDisplay.textContent = buttonValue;
+const operations = document.querySelectorAll('.operation');
+
+operations.forEach((operation) => {
+    operation.addEventListener('click', () => {
+        if (operatorValue === undefined) {
+            result = Number(displayValue.textContent);
+            operatorValue = operation.textContent;
         } else {
-            calcDisplay.textContent += buttonValue;
-        } 
-    });
-});
+            switch (operatorValue) {
+                case '/':
+                    result = operate(result, displayValue.textContent, divide);
+                    break;
+                case '*':
+                    result = operate(result, displayValue.textContent, multiply);
+                    break;
+                case '-':
+                    result = operate(result, displayValue.textContent, subtract);
+                    break;
+                case '+':
+                    result = operate(result, displayValue.textContent, add);
+                    break;
+                case '=':
+                    switch (operatorValue) {
+                        case '/':
+                            result = operate(result, displayValue.textContent, divide);
+                            break;
+                        case '*':
+                            result = operate(result, displayValue.textContent, multiply);
+                            break;
+                        case '-':
+                            result = operate(result, displayValue.textContent, subtract);
+                            break;
+                        case '+':
+                            result = operate(result, displayValue.textContent, add);
+                            break;
+                    }
+            }
+            displayValue.textContent = result;
+            operatorValue = undefined;
+        }
+        newInputState = 0;
+    })
+})
